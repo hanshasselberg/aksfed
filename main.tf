af53dbd6ed3e67609b020a9cf6465be30a653b99
Hans Hasselberg <me@hans.io>
2020-11-18 12:59:29 +0100

    more

--- main.tf
+++ main.tf
@@ -41,12 +41,16 @@ resource "azurerm_virtual_network_peering" "peer1to2" {
   resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet1.name
   remote_virtual_network_id = azurerm_virtual_network.vnet2.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
 }
 resource "azurerm_virtual_network_peering" "peer2to1" {
   name                      = "peer2to1"
   resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet2.name
   remote_virtual_network_id = azurerm_virtual_network.vnet1.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
 }
 
 resource "azurerm_kubernetes_cluster" "dc1" {
