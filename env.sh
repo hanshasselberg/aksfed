158a3fab59221f96f822ea6ac531cd06f48eec71
Hans Hasselberg <me@hans.io>
2021-01-18 10:05:10 +0100

    use hcs provider

--- env.sh
+++ env.sh
@@ -4,5 +4,4 @@ set -e
 set -x
 
 export HCP_PLAN=hcs-integration-preview
-export RG=federation-test-hans-3
-export TF_VAR_rg=$RG
+export RG=`terraform output rg`
--- main.tf
+++ main.tf
@@ -1,53 +1,106 @@
-variable rg {}
-
-locals {
-  dc1data = jsondecode(file("${path.module}/${var.rg}-hcs-dc1.json"))
-  dc2data = jsondecode(file("${path.module}/${var.rg}-hcs-dc2.json"))
-  dc3data = jsondecode(file("${path.module}/${var.rg}-hcs-dc3.json"))
-}
-
 provider "azurerm" {
   version = "~>2.0"
   features {}
+}
+
+provider "hcs" { }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "federation-test-hans-1"
+  location = "westus2"
+}
+
+output "rg" {
+  value = azurerm_resource_group.rg.name
+}
+output "url" {
+  value = hcs_cluster.dc1.consul_external_endpoint_url
+}
+output "token" {
+  value = hcs_cluster.dc1.consul_root_token_secret_id
+}
+
+// Create dc1
+resource "hcs_cluster" "dc1" {
+  resource_group_name      = azurerm_resource_group.rg.name
+  managed_application_name = "dc1"
+  email                    = "hans@hashicorp.com"
+  cluster_mode             = "production"
+  min_consul_version       = "v1.9.1"
+  vnet_cidr                = "172.25.16.0/24"
+  consul_datacenter        = "dc1"
+  consul_external_endpoint = true
+}
+
+// Create a federation token
+data "hcs_federation_token" "dc1" {
+  resource_group_name      = hcs_cluster.dc1.resource_group_name
+  managed_application_name = hcs_cluster.dc1.managed_application_name
+}
+
+// Create dc2
+resource "hcs_cluster" "dc2" {
+  resource_group_name      = azurerm_resource_group.rg.name
+  managed_application_name = "dc2"
+  email                    = "hans@hashicorp.com"
+  cluster_mode             = "production"
+  min_consul_version       = "v1.9.1"
+  vnet_cidr                = "172.25.17.0/24"
+  consul_datacenter        = "dc2"
+  consul_federation_token  = data.hcs_federation_token.dc1.token
+  consul_external_endpoint = true
+}
+
+// Create dc3
+resource "hcs_cluster" "dc3" {
+  resource_group_name      = azurerm_resource_group.rg.name
+  managed_application_name = "dc3"
+  email                    = "hans@hashicorp.com"
+  cluster_mode             = "production"
+  min_consul_version       = "v1.9.1"
+  vnet_cidr                = "172.25.18.0/24"
+  consul_datacenter        = "dc3"
+  consul_federation_token  = data.hcs_federation_token.dc1.token
+  consul_external_endpoint = true
 }
 
 # Network setup
 # vnet1, vnet2, vnet3 with subnets, peered together
 resource "azurerm_virtual_network" "vnet1" {
   name                = "vnet1"
-  location            = "westus2"
-  resource_group_name = var.rg
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/8"]
 }
 resource "azurerm_subnet" "subnet1" {
   name                 = "subnet"
-  resource_group_name  = var.rg
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.0.0.0/16"]
 }
 
 resource "azurerm_virtual_network" "vnet2" {
   name                = "vnet2"
-  location            = "westus2"
-  resource_group_name = var.rg
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["11.0.0.0/8"]
 }
 resource "azurerm_subnet" "subnet2" {
   name                 = "subnet"
-  resource_group_name  = var.rg
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet2.name
   address_prefixes     = ["11.0.0.0/16"]
 }
 
 resource "azurerm_virtual_network" "vnet3" {
   name                = "vnet3"
-  location            = "westus2"
-  resource_group_name = var.rg
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["12.0.0.0/8"]
 }
 resource "azurerm_subnet" "subnet3" {
   name                 = "subnet"
-  resource_group_name  = var.rg
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet3.name
   address_prefixes     = ["12.0.0.0/16"]
 }
@@ -55,7 +108,7 @@ resource "azurerm_subnet" "subnet3" {
 # peer AKS vnets
 resource "azurerm_virtual_network_peering" "aks1_to_aks2" {
   name                      = "aks1_to_aks2"
-  resource_group_name       = var.rg
+  resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet1.name
   remote_virtual_network_id = azurerm_virtual_network.vnet2.id
   allow_virtual_network_access = true
@@ -63,7 +116,7 @@ resource "azurerm_virtual_network_peering" "aks1_to_aks2" {
 }
 resource "azurerm_virtual_network_peering" "aks2_to_aks1" {
   name                      = "aks2_to_aks1"
-  resource_group_name       = var.rg
+  resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet2.name
   remote_virtual_network_id = azurerm_virtual_network.vnet1.id
   allow_virtual_network_access = true
@@ -71,7 +124,7 @@ resource "azurerm_virtual_network_peering" "aks2_to_aks1" {
 }
 resource "azurerm_virtual_network_peering" "aks1_to_aks3" {
   name                      = "aks1_to_aks3"
-  resource_group_name       = var.rg
+  resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet1.name
   remote_virtual_network_id = azurerm_virtual_network.vnet3.id
   allow_virtual_network_access = true
@@ -79,7 +132,7 @@ resource "azurerm_virtual_network_peering" "aks1_to_aks3" {
 }
 resource "azurerm_virtual_network_peering" "aks3_to_aks1" {
   name                      = "aks3_to_aks1"
-  resource_group_name       = var.rg
+  resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet3.name
   remote_virtual_network_id = azurerm_virtual_network.vnet1.id
   allow_virtual_network_access = true
@@ -87,7 +140,7 @@ resource "azurerm_virtual_network_peering" "aks3_to_aks1" {
 }
 resource "azurerm_virtual_network_peering" "aks2_to_aks3" {
   name                      = "aks2_to_aks3"
-  resource_group_name       = var.rg
+  resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet2.name
   remote_virtual_network_id = azurerm_virtual_network.vnet3.id
   allow_virtual_network_access = true
@@ -95,7 +148,7 @@ resource "azurerm_virtual_network_peering" "aks2_to_aks3" {
 }
 resource "azurerm_virtual_network_peering" "aks3_to_aks2" {
   name                      = "aks3_to_aks2"
-  resource_group_name       = var.rg
+  resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet3.name
   remote_virtual_network_id = azurerm_virtual_network.vnet2.id
   allow_virtual_network_access = true
@@ -104,12 +157,12 @@ resource "azurerm_virtual_network_peering" "aks3_to_aks2" {
 
 # peer to HCS
 data "azurerm_virtual_network" "hcsvnet1" {
-  name                = "${local.dc1data.outputs.vnet_name.value}-vnet"
-  resource_group_name = "${var.rg}-mrg-dc1"
+  name                = hcs_cluster.dc1.vnet_name
+  resource_group_name = hcs_cluster.dc1.managed_resource_group_name
 }
 resource "azurerm_virtual_network_peering" "aks1_to_hcs1" {
   name                      = "aks1_to_hcs1"
-  resource_group_name       = var.rg
+  resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet1.name
   remote_virtual_network_id = data.azurerm_virtual_network.hcsvnet1.id
   allow_virtual_network_access = true
@@ -117,19 +170,19 @@ resource "azurerm_virtual_network_peering" "aks1_to_hcs1" {
 }
 resource "azurerm_virtual_network_peering" "hcs1_to_aks1" {
   name                      = "hcs1_to_aks1"
-  resource_group_name       = "${var.rg}-mrg-dc1"
+  resource_group_name       = hcs_cluster.dc1.managed_resource_group_name
   virtual_network_name      = data.azurerm_virtual_network.hcsvnet1.name
   remote_virtual_network_id = azurerm_virtual_network.vnet1.id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
 }
 data "azurerm_virtual_network" "hcsvnet2" {
-  name                = "${local.dc2data.outputs.vnet_name.value}-vnet"
-  resource_group_name = "${var.rg}-mrg-dc2"
+  name                = hcs_cluster.dc2.vnet_name
+  resource_group_name = hcs_cluster.dc2.managed_resource_group_name
 }
 resource "azurerm_virtual_network_peering" "aks2_to_hcs2" {
   name                      = "aks2_to_hcs2"
-  resource_group_name       = var.rg
+  resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet2.name
   remote_virtual_network_id = data.azurerm_virtual_network.hcsvnet2.id
   allow_virtual_network_access = true
@@ -137,19 +190,19 @@ resource "azurerm_virtual_network_peering" "aks2_to_hcs2" {
 }
 resource "azurerm_virtual_network_peering" "hcs2_to_aks2" {
   name                      = "hcs2_to_aks2"
-  resource_group_name       = "${var.rg}-mrg-dc2"
+  resource_group_name       = hcs_cluster.dc2.managed_resource_group_name
   virtual_network_name      = data.azurerm_virtual_network.hcsvnet2.name
   remote_virtual_network_id = azurerm_virtual_network.vnet2.id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
 }
 data "azurerm_virtual_network" "hcsvnet3" {
-  name                = "${local.dc3data.outputs.vnet_name.value}-vnet"
-  resource_group_name = "${var.rg}-mrg-dc3"
+  name                = hcs_cluster.dc3.vnet_name
+  resource_group_name = hcs_cluster.dc3.managed_resource_group_name
 }
 resource "azurerm_virtual_network_peering" "aks3_to_hcs3" {
   name                      = "aks3_to_hcs3"
-  resource_group_name       = var.rg
+  resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet3.name
   remote_virtual_network_id = data.azurerm_virtual_network.hcsvnet3.id
   allow_virtual_network_access = true
@@ -157,7 +210,7 @@ resource "azurerm_virtual_network_peering" "aks3_to_hcs3" {
 }
 resource "azurerm_virtual_network_peering" "hcs3_to_aks3" {
   name                      = "hcs3_to_aks3"
-  resource_group_name       = "${var.rg}-mrg-dc3"
+  resource_group_name       = hcs_cluster.dc3.managed_resource_group_name
   virtual_network_name      = data.azurerm_virtual_network.hcsvnet3.name
   remote_virtual_network_id = azurerm_virtual_network.vnet3.id
   allow_virtual_network_access = true
@@ -170,8 +223,8 @@ resource "azurerm_kubernetes_cluster" "dc1" {
     type = "SystemAssigned"
   }
   name                = "dc1"
-  location            = "westus2"
-  resource_group_name = var.rg
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "dc1-dns"
 
   linux_profile {
@@ -203,8 +256,8 @@ resource "azurerm_kubernetes_cluster" "dc2" {
     type = "SystemAssigned"
   }
   name                = "dc2"
-  location            = "westus2"
-  resource_group_name = var.rg
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "dc2-dns"
 
   linux_profile {
@@ -236,8 +289,8 @@ resource "azurerm_kubernetes_cluster" "dc3" {
     type = "SystemAssigned"
   }
   name                = "dc3"
-  location            = "westus2"
-  resource_group_name = var.rg
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "dc3-dns"
 
   linux_profile {
